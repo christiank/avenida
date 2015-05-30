7ffb0ca264cc70e9fa67c4281a430735a95c8f86
Christian Koch <cfkoch@sdf.lonestar.org>
2015-05-30 14:26:23 -0700

    Add a util library. Starting with cd() and pwd().

---
+++ src/avnscript-util.c
@@ -0,0 +1,56 @@
+/*
+ * vim: noet
+ *
+ * avnscript-util.c
+ * Christian Koch <cfkoch@sdf.lonestar.org>
+ */
+
+#include <limits.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <lua.h>
+#include <lauxlib.h>
+
+#include "avnscript-util.h"
+
+static int avenida_cd(lua_State *);
+static int avenida_pwd(lua_State *);
+
+static int
+avenida_cd(lua_State *L)
+{
+	char *path;
+
+	path = (char *)luaL_checkstring(L, 1);
+	if (chdir(path) == -1)
+		luaL_error(L, "couldn't change directory");
+
+	return 0;
+}
+
+
+static int
+avenida_pwd(lua_State *L)
+{
+	char *s;
+
+	s = getwd(NULL);
+	lua_pushstring(L, s);
+	return 1;
+}
+
+
+int
+luaopen_util(lua_State *L)
+{
+	luaL_Reg funcs[] = {
+		{"cd", avenida_cd},
+		{"pwd", avenida_pwd},
+		{NULL, NULL}
+	};
+
+	luaL_newlib(L, funcs);
+	luaL_newmetatable(L, "util");
+	return 2;
+}
---
+++ src/avnscript-util.h
@@ -0,0 +1,15 @@
+/*
+ * vim: noet
+ *
+ * avnscript-util.h
+ * Christian Koch <cfkoch@sdf.lonestar.org>
+ */
+
+#ifndef AVENIDA_AVNSCRIPT_UTIL_H
+#define AVENIDA_AVNSCRIPT_UTIL_H
+
+#include <lua.h>
+
+int luaopen_util(lua_State *);
+
+#endif /* AVENIDA_AVNSCRIPT_UTIL_H */
